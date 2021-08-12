e6592465b16e6d27a32eb49c4ce924d08fef6421
TseniukhAnton <anton.tseniukh@gmail.com>
2021-08-12 14:44:53 +0300

    arrays added

---
+++ src/Arrays/Main.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add(0,"Elefant");
+        list.add(1,"Horse");
+        list.add(2,"Lion");
+        list.add(3,"Puma");
+        list.add(4,"Tiger");
+        list.add(5,"Possum");
+        list.add(6,"Hippo");
+        list.add(7,"Chicken");
+    }
+}
---
+++ src/Arrays/Zoo.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    ArrayList<String> list = new ArrayList<>();
+
+
+}
